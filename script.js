c8547f070b41ed7642cda96ed4999fcdf0592d90
Leslie Karen <LeslieKaren@gmail.com>
2025-12-17 09:19:39 +0100

    modification de la page script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Ce code s'exécute au "Start" (chargement complet de la page)
-window.addEventListener('DOMContentLoaded', () => {
-    console.log("La page de Leslie est prête !");
-    
-    // Exemple d'interactivité : Un message de bienvenue
-    alert("Bienvenue sur la page de Leslie - Groupe N°4");
-});
